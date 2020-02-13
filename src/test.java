bb7fe27b18956c64cacdeacb3a3297f2f50ae21a
Jiri Vanhouwe <jiri.vanhouwe@student.hogent.be>
2020-02-13 15:34:20 +0100

    Belangrijke startfunctie.

--- src/test.java
+++ src/test.java
@@ -1,11 +1,20 @@
+import java.security.SecureRandom;
 
 public class test {
  //test commit
 	//test commit Artuur
-
-	int getal = 1;
-
-	
 	//duizend bommen en granaten
-
+	
+	
+	public static void main(String[] args) {
+		System.out.println(runDezeFunctie());
+	}
+	
+	public static String runDezeFunctie() {
+		SecureRandom random = new SecureRandom();
+		int score = random.nextInt(20);
+		
+		return (score < 10)? String.format("Je zal %d/20 behalen.%nDit is niveau 'Foulon'! Niet goed!%n", score):String.format("Je zal %d/20 behalen.%nDit is niveau 'Ramon'! Proficiat!%n", score);
+	
+	}
 }
