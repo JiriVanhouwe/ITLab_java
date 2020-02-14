a60453dfded7f1cb0097d61161c08692f76415c4
Robbe Ramon <robbe.ramon@student.hogent.be>
2020-02-14 11:59:32 +0100

    Minor change to test function

--- src/test.java
+++ src/test.java
@@ -12,7 +12,11 @@ public class test {
 	
 	public static String runDezeFunctie() {
 		SecureRandom random = new SecureRandom();
-		int score = random.nextInt(20);
+		//int score = random.nextInt(20);
+		int score = 8;
+		
+		if (score == 8)
+			return String.format("Je zal %d/20 behalen, vergeet niet naar de feedback te gaan ;)", score);
 		
 		return (score < 10)? String.format("Je zal %d/20 behalen.%nDit is niveau 'Foulon'! Niet goed!%n", score):String.format("Je zal %d/20 behalen.%nDit is niveau 'Ramon'! Proficiat!%n", score);
 	
