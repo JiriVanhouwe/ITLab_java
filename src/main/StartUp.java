91b2c8dcbe83fbc70b1463b222f40ffc7dd9669d
Jiri Vanhouwe <jiri.vanhouwe@student.hogent.be>
2020-02-18 19:22:25 +0100

    Klasse Persistentiecontroller gemaakt.

---
+++ src/main/StartUp.java
@@ -0,0 +1,14 @@
+package main;
+
+import domain.PersistentieController;
+
+public class StartUp {
+
+	public static void main(String[] args) {
+
+		new PersistentieController();
+		System.out.println("---Database created.---");
+
+	}
+
+}
