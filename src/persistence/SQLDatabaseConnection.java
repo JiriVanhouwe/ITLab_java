47e5c672e37e34081e9d9fb404583ba9ad28bc83
Artuur Everaert <artuur.everaert@student.hogent.be>
2020-03-04 10:44:10 +0100

    Databank at Azure connected

--- src/persistence/SQLDatabaseConnection.java
+++ src/persistence/SQLDatabaseConnection.java
@@ -10,10 +10,10 @@ public class SQLDatabaseConnection {
     // Connect to your database.
     // Replace server name, username, and password with your credentials
     public static void main(String[] args) {
-    	final String IP_SERVER = "51.144.159.116";
-    	final String DB_NAME ="testjava";
-    	final String DB_USERNAME = "sebastiaan.labijn";
-    	final String DB_PWD = "2.f17302537U";
+    	final String IP_SERVER = "51.137.57.86";//"51.144.159.116";
+    	final String DB_NAME ="ITLab_db";
+    	final String DB_USERNAME = "artuur.everaert";
+    	final String DB_PWD = "1D5a34_36fF8";
     	
         String connectionUrl =
                 "jdbc:sqlserver://"+IP_SERVER+ ":1433;"
@@ -30,10 +30,10 @@ public class SQLDatabaseConnection {
 
         try (Connection connection = DriverManager.getConnection(connectionUrl);
                 Statement statement = connection.createStatement();) {
-
+        	System.out.println("binnen!");
             // Create and execute a SELECT SQL statement.
-            String selectSql = "SELECT * from dbo.Gebruiker";
-            resultSet = statement.executeQuery(selectSql);
+           // String selectSql = "SELECT * from dbo.Gebruiker";
+           // resultSet = statement.executeQuery(selectSql);
 
             // Print results from select statement
             while (resultSet.next()) {
